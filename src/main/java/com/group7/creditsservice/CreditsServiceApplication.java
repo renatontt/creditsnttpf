dd8ed03b21e1dd37b69211590ddd40b378ab7594
Renato <renato.garay@pucp.pe>
2022-05-03 17:40:32 -0500

    first commit

---
+++ src/main/java/com/group7/creditsservice/CreditsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.group7.creditsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditsServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group7/creditsservice/CreditsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group7.creditsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
